2bd05868eccf090693bc1be05b5aa602f61c288f
Rahul Lal <Rahul21Lal@gmail.com>
2026-03-02 09:40:17 +1300

    Inital Commit of project

---
+++ burger-national-V1/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace burger_national_V1
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
