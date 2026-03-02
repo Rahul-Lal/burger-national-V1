c977b098d50c2fda4d9ab4b305411ff60d213c17
Rahul Lal <Rahul21Lal@gmail.com>
2026-03-02 21:20:45 +1300

    Remade homepage to fix build. Original page wasn't named properly.

--- burger-national-V1/Homepage.xaml.cs
+++ burger-national-V1/Homepage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -12,11 +14,11 @@ using System.Windows.Shapes;
 namespace burger_national_V1
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Homepage.xaml
     /// </summary>
-    public partial class MainWindow : NavigationWindow
+    public partial class Homepage : Page
     {
-        public MainWindow()
+        public Homepage()
         {
             InitializeComponent();
         }
